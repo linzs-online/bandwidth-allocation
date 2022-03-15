5c45ffd03588c5a2022405eec1c9a74c2a81330f
linzs-online <linzs.online@gmail.com>
2022-03-15 17:39:53 +0800

    dataDeal

--- CodeCraft-2022/src/CodeCraft-2022.cpp
+++ CodeCraft-2022/src/CodeCraft-2022.cpp
@@ -6,18 +6,19 @@
 #include <sstream>
 
 using namespace std;
+string temp,line;
 
 class base{
 private:
-    unordered_map<string,int> siteBandWith;
-    vector<string> demandName;
-    vector<vector<int>> demand;
-    unordered_map<string, vector<int>> qos;
+    vector<string> demandNode;
+    vector<string> siteNode;
+    unordered_map<string,int> siteBandWith;  //边缘节点的带宽信息
+    vector<vector<int>> demand;  //时间序列下的客户节点请求
+    unordered_map<string, vector<int>> site2demand;
     int qos_constraint;
     vector<vector<pair<string, int>>> result;
-
 public:
-    base(string&& _filePath){};
+    base(string&& _filePath);
     void solve();
     void save();
     ~base();
@@ -32,51 +33,85 @@ base::base(string&& _filePath){
     // 初始化siteNodo（ ）
     siteNodeInit(_filePath + "site_bandwidth.csv");
     // 初始化demand（）
-
+    demandNodeInit(_filePath + "demand.csv");
     // 初始化QOS()
-
+    qosInit(_filePath + "qos.csv");
     // 初始化config（）
-
+    qosConstraintInit(_filePath + "config.ini");
 }
-
-base::qosConstraintInit(string&& _filePath){
+base::~base(){
     
 }
 
-void siteNodeInit(string& _filePath);
-void creatSiteNodeData(string& _filePath, unordered_map<string,int>& _site_bandwidth);
-int getSiteNodeBandWith(string& _name, unordered_map<string,int>& _site_bandwidth);
+void base::siteNodeInit(string&& _filePath){
+    std::ifstream site_bandwidthfile(_filePath, std::ios::in);
+    std::string line,siteNoteName,siteNodeBW;
+    getline(site_bandwidthfile,temp);
+    while(getline(site_bandwidthfile,line)){
+        istringstream sin(line); //将整行字符串line读入到字符串流istringstream中
+        getline(sin, siteNoteName, ',');
+        siteNode.push_back(siteNoteName);  // 获得边缘节点名字序列
+        sin >> siteNodeBW;
+        pair<string,int> _frame(siteNoteName, atoi(siteNodeBW.c_str()));
+        siteBandWith.insert(_frame);  // 把边缘节点和它的带宽封装成unordermap
+    };
+    
+    cout << "siteNodeBandWith Inited " <<  siteBandWith.at("9") << endl;
+}
+
+void base::demandNodeInit(string&& _filePath){
+    std::ifstream demandfile(_filePath, std::ios::in);
+    getline(demandfile, line);
+    istringstream sin(line);
+    getline(sin, temp, ',');
+    while(getline(sin, temp, ','))
+    {
+        demandNode.emplace_back(temp); // 获得客户节点的名字序列
+    }
+    vector<int> demandFrame;
+    while(getline(demandfile,line)){
+        istringstream sin(line);
+        getline(sin, temp, ',');
+        while(getline(sin, temp, ',')){
+            demandFrame.emplace_back(atoi(temp.c_str()));
+        }
+        demand.emplace_back(demandFrame); //将所有时间序列的客户节点请求封装成vector
+        demandFrame.clear();
+    }
+    cout << "demandNode Inited " << demandNode.size() << endl;
+}
+
+void base::qosInit(string&& _filePath){
+    std::ifstream qosfile(_filePath, std::ios::in);
+    getline(qosfile, temp);
+    vector<int> preSite2demand;
+    string preSiteNode;
+    while(getline(qosfile, line))
+    {
+        istringstream sin(line);
+        getline(sin, preSiteNode, ',');
+        while(getline(sin, temp, ',')){
+            preSite2demand.emplace_back(atoi(temp.c_str()));
+        }
+        pair<string,vector<int>> pre_site2demand(preSiteNode,preSite2demand);
+        site2demand.insert(pre_site2demand);
+        preSite2demand.clear();
+    }
+    cout << "preSite2demand Inited " << site2demand.at("A")[2] << endl;
+}
+
+void base::qosConstraintInit(string&& _filePath){
+    std::ifstream qos_constraintfile(_filePath, std::ios::in);
+    while(getline(qos_constraintfile,temp,'='));
+    qos_constraint = atoi(temp.c_str());
+    cout << "qos_constraint = " <<  qos_constraint << endl;
+}
 
 
 int main() {
-    // 边缘节点信息
-    cout << "output_test" << endl;
-    string site_bandwidth_filePath = "../data/site_bandwidth.csv";
-    string demand_filePath = "../data/demand.csv";
-    unordered_map<string,int> site_bandwidth;
-    creatSiteNodeData(site_bandwidth_filePath, site_bandwidth);
+ 
 
-    string line;
-    std::ifstream _data(demand_filePath, std::ios::in);
-    while (std::getline(_data,line))
-    {
-        istringstream sin(line); //将整行字符串line读入到字符串流istringstream中
-        while (getline(sin,))
-        {
-            /* code */
-        }
-        
-    }
-    
-    cout << site_bandwidth.at("AJ") << endl;
-    // istringstream sin(line); //将整行字符串line读入到字符串流istringstream中
-    // getline(sin, siteName, ',');
-    // sin >> bandwith;
-    // pair<string,int> Frame (siteName, bandwith);
-    // site_bandwidth.insert(Frame);
-    // /* code */
-    
-    // cout << site_bandwidth.size() << endl;
+    base dataInit("../data/");
 	return 0;
 }
 
@@ -103,10 +138,5 @@ void creatSiteNodeData(string& _filePath, unordered_map<string,int>& _site_bandw
     }
 }
 
-int getSiteNodeBandWith(string& _name, unordered_map<string,int>& _site_bandwidth){
-    return _site_bandwidth.at(_name);
-}
 
-void siteNodeInit(string&& _filePath){
-    
-}
+
--- build/CMakeFiles/CodeCraft-2022.dir/cmake_clean.cmake
+++ build/CMakeFiles/CodeCraft-2022.dir/cmake_clean.cmake
@@ -2,7 +2,6 @@ file(REMOVE_RECURSE
   "/home/lin/Downloads/Compressed/SDK/SDK_C++/bin/CodeCraft-2022"
   "/home/lin/Downloads/Compressed/SDK/SDK_C++/bin/CodeCraft-2022.pdb"
   "CMakeFiles/CodeCraft-2022.dir/CodeCraft-2022.cpp.o"
-  "CMakeFiles/CodeCraft-2022.dir/dataBase.cpp.o"
 )
 
 # Per-language clean rules from dependency scanning.
