7041f3fe96e21105bcb73e9d3f4e62b41979cc73
linzs-online <linzs.online@gmail.com>
2022-03-15 20:37:21 +0800

    add a demand2site

--- CodeCraft-2022/src/CodeCraft-2022.cpp
+++ CodeCraft-2022/src/CodeCraft-2022.cpp
@@ -16,6 +16,7 @@ private:
     unordered_map<string,int> siteBandWith;  //边缘节点的带宽信息
     vector<vector<int>> demand;  //时间序列下的客户节点请求
     unordered_map<string, vector<int>> site2demand;
+    unordered_map<string, vector<int>> demand2site;
     int qos_constraint;
     vector<vector<pair<string, int>>> result;
 public:
@@ -47,7 +48,7 @@ base::~base(){
 void base::siteNodeInit(string&& _filePath){
     std::ifstream site_bandwidthfile(_filePath, std::ios::in);
     std::string line,siteNoteName,siteNodeBW;
-    getline(site_bandwidthfile,temp);
+    getline(site_bandwidthfile, temp);
     int siteNodeBW_int;
     while(getline(site_bandwidthfile,line)){
         istringstream sin(line); //将整行字符串line读入到字符串流istringstream中
@@ -65,7 +66,7 @@ void base::siteNodeInit(string&& _filePath){
 
 void base::demandNodeInit(string&& _filePath){
     std::ifstream demandfile(_filePath, std::ios::in);
-    getline(demandfile, line);
+    getline(demandfile, line,'\r');
     istringstream sin(line);
     getline(sin, temp, ',');
     while(getline(sin, temp, ','))
@@ -101,6 +102,18 @@ void base::qosInit(string&& _filePath){
         site2demand.insert(pre_site2demand);
         preSite2demand.clear();
     }
+    vector<int> preDemand2site;
+    auto index = 0;
+    for(auto dd: demandNode){
+        for(auto ss : siteNode){
+            preDemand2site.emplace_back(site2demand.at(ss)[index]);
+        }
+        index++;
+        pair<string,vector<int>> pre_demand2site(dd,preDemand2site);
+        demand2site.insert(pre_demand2site);
+        preDemand2site.clear();
+    }
+
     cout << "preSite2demand Inited " << site2demand.at("A")[2] << endl;
 }
 
@@ -113,7 +126,7 @@ void base::qosConstraintInit(string&& _filePath){
 
 
 int main() {
-    base dataInit("../data/");
+    base dataInit("../../data/");
 	return 0;
 }
 
