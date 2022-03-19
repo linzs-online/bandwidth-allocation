873f97aa2170d51ac47fe44e41150fe42edc188b
linzs-online <linzs.online@gmail.com>
2022-03-19 21:23:13 +0800

    增加了边缘节点使用记录功能

--- CodeCraft-2022/inc/paramter.h
+++ CodeCraft-2022/inc/paramter.h
@@ -4,7 +4,7 @@
 #include <vector>
 #include <numeric>
 
-
+using namespace std;
 class Paramerter {
 public:
     using Ptr = std::shared_ptr<Paramerter>;
@@ -15,3 +15,14 @@ public:
     void normal(double mean = 0, double stddev = 1);
 };
 
+
+class SiteLog{
+public:
+    unordered_map<string,pair<int,vector<double>>> siteLogMap;
+    unordered_map<string,int> sitBandWithMap;
+    vector<string> siteNodeName;
+
+    SiteLog(vector<string> _siteNodeName, vector<int> _siteNodeBandWith, int _frameSize);
+    void write2Log(vector<vector<pair<string, int>>>::iterator itFirst,
+                    vector<vector<pair<string, int>>>::iterator itEnd, int frameID);
+};
--- CodeCraft-2022/src/paramter.cpp
+++ CodeCraft-2022/src/paramter.cpp
@@ -1,4 +1,6 @@
 #include <random>
+#include <unordered_map>
+#include <unordered_set>
 #include "../inc/paramter.h"
 
 
@@ -32,5 +34,44 @@ void Paramerter::normal(double mean, double stddev) {
     std::normal_distribution<> rng{mean, stddev};
     for (size_t i = 0; i < value.size(); i++) {
         value[i] = std::abs(rng(gen));
+    }
+}
+
+//unordered_map<string,pair<int,vector<double>>> siteLogMap;
+SiteLog::SiteLog(vector<string> _siteNodeName, vector<int> _siteNodeBandWith, int _frameSize):siteNodeName(_siteNodeName){
+    //边缘节点的带宽，用于计算每个边缘节点的利用率
+    for(auto i = 0; i < _siteNodeBandWith.size(); i++){
+        pair<string,int> _temp(_siteNodeName[i], _siteNodeBandWith[i]);
+        sitBandWithMap.insert(_temp);
+    }
+    vector<double> perSiteNodeUsage(_frameSize,0);
+    for(auto& nodeName: _siteNodeName){
+        pair<int, vector<double>> _pair(0,perSiteNodeUsage);
+        pair<string,pair<int,vector<double>>> _history(nodeName,_pair);
+        siteLogMap.insert(_history);
+    }
+}
+//unordered_map<string,pair<int,vector<double>>> siteLogMap;
+void SiteLog::write2Log(vector<vector<pair<string, int>>>::iterator itFirst,
+                        vector<vector<pair<string, int>>>::iterator itEnd, int frameID){
+    //记录每个边缘节点在这一帧中的带宽分配总量
+    unordered_map<string, int> nowFrameSiteUsed;
+    //记录这一帧使用过的边缘节点的名字
+    unordered_set<string> nowFrameSiteName;
+    for(auto& name : siteNodeName){
+        pair<string,int> _temp(name,0);
+        nowFrameSiteUsed.insert(_temp);
+    }
+    for(auto it = itEnd; it != itFirst; it--){
+        for(auto perDresut = it->begin(); perDresut != it->end(); perDresut++){
+            nowFrameSiteName.insert(perDresut->first);
+            nowFrameSiteUsed[perDresut->first] += perDresut->second;
+        }
+    }
+    for(auto siteName : nowFrameSiteName){
+        siteLogMap.at(siteName).first++;
+        double x = nowFrameSiteUsed.at(siteName); //隐式转换
+        double y = sitBandWithMap.at(siteName);
+        siteLogMap.at(siteName).second[frameID] = x/y; //返回使用率
     }
 }
